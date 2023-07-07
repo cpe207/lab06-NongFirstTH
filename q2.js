88f503b592b5177ff7d67d0cee03261194c20d79
NongFirstTH <TanabodeeChannel@gmail.com>
2023-07-07 23:50:52 +0700

    V2

--- q2.js
+++ q2.js
@@ -5,12 +5,12 @@ const getTodo = async (todoId) => {
     const resp = await axios.get("https://jsonplaceholder.typicode.com/todos/"+todoId)
     const resp2 = await axios.get("https://jsonplaceholder.typicode.com/users/"+resp.data.userId)
     const result = {}
-    const owner = resp2.data.name
+    /*const owner = resp2.data.name
     const title = resp.data.title
-    const completed = resp.data.completed
-    result.owner = owner
-    result.title = title
-    result.completed = completed
+    const completed = resp.data.completed*/
+    result.owner = resp2.data.name
+    result.title = resp.data.title
+    result.completed = resp.data.completed
   return  result
   }catch (error){
     return 'INVALID TODO ID'
